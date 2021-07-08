8c3f3d09cb8dc759628c494deb81627b3dce49dd
엄청난노옴 <lyutvs@gmail.ocm>
2021-07-08 19:07:37 +0900

    ⚡️ :: Oauth2AuthorizationConfig

--- src/main/java/com/example/authorizationserver/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/example/authorizationserver/config/Oauth2AuthorizationConfig.java
@@ -1,17 +1,39 @@
 package com.example.authorizationserver.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+import javax.sql.DataSource;
+
+
+/**
+ * id/password 기반 Oauth2 인증을 담당하는 서버
+ * 다음 endpont가 자동 생성 된다.
+ * /oauth/authorize
+ * /oauth/token
+ */
+@RequiredArgsConstructor
+@Configuration
+@EnableAuthorizationServer
 public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("testClientID")
-                .secret("testSecret")
-                .redirectUris("http://localhost:8081/oauth2/callback")
-                .authorizedGrantTypes("authorization_code")
-                .scopes("read", "writr")
-                .accessTokenValiditySeconds(30000);
-    }
 
+    @Autowired
+    private DataSource dataSource;
+    private PasswordEncoder passwordEncoder;
+
+    /**
+     * 클라이언트 정보 주입 방식을 jdbcdetail로 변경
+     *
+     */
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
+    }
 }
