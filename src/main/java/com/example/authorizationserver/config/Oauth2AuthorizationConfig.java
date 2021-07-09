d8c8b02fc69f8bd11501680a33aed270b7be66dc
엄청난노옴 <lyutvs@gmail.ocm>
2021-07-09 22:43:56 +0900

    ⚡️ :: Oauth2AuthorizationConfig

--- src/main/java/com/example/authorizationserver/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/example/authorizationserver/config/Oauth2AuthorizationConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import javax.sql.DataSource;
 
@@ -41,13 +42,32 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
             clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
     }
 
-
     /**
      * 토큰 정보를 DB를 통해 관리한다.
+     *
+     * @return
      */
+
+    //    @Override
+    //    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    //        endpoints.tokenStore(new JdbcTokenStore(dataSource));
+    //    }
+
+    /**
+     * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
+     *
+     * @param endpoints
+     * @throws Exception
+     */
+
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.tokenStore(new JdbcTokenStore(dataSource));
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            super.configure(endpoints);
+            endpoints.accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    private AccessTokenConverter jwtAccessTokenConverter() {
+        return new JwtAccessTokenConverter();
     }
 
 
