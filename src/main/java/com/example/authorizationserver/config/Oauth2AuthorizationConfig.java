1919b0f0c27731e1993ef900fd1a802250ff6c8a
엄청난노옴 <lyutvs@gmail.ocm>
2021-07-10 18:39:40 +0900

    ⚡️ :: Oauth2AuthorizationConfig

--- src/main/java/com/example/authorizationserver/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/example/authorizationserver/config/Oauth2AuthorizationConfig.java
@@ -1,17 +1,24 @@
 package com.example.authorizationserver.config;
 
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
 
@@ -28,49 +35,84 @@ import javax.sql.DataSource;
 
 public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
     private DataSource dataSource;
     private PasswordEncoder passwordEncoder;
+    private final CustomUserDetailService userDetailService;
 
+    @Value("${security.oauth2.jwt.singkey}")
+    private String singkey;
     /**
      * 클라이언트 정보 주입 방식을 jdbcdetail로 변경
      *
      */
 
     @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-            clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()") //allow check token
+                .allowFormAuthenticationForClients();
     }
 
     /**
-     * 토큰 정보를 DB를 통해 관리한다.
-     *
-     * @return
+     * 클라이언트 정보 주입 방식을 jbdcdetail로 변경
      */
 
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
+    }
+    /**
+     * 토큰 정보를 DB를 통해 관리한다.
+     *
+     *
+     */
+    // @Override
+    // public void config(AuthorizationServerEndpointsConfigurer endpoints) {
+    //    endpoints.tokenStore(new JdbcTokenStore(dataSource)).userDetailsService(userDetailService);
+    //}
+
     //    @Override
-    //    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    //    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
     //        endpoints.tokenStore(new JdbcTokenStore(dataSource));
     //    }
 
     /**
      * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
-     *
-     * @param endpoints
-     * @throws Exception
      */
-
-    private final CustomUserDetailService userDetailService;
-
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
             super.configure(endpoints);
             endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
     }
 
-    private AccessTokenConverter jwtAccessTokenConverter() {
-        return new JwtAccessTokenConverter();
-    }
-    
+    /**
+     * jwt converter - signKey 공유 방식
+     */
+//    @Bean
+//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        converter.setSigningKey(signKey);
+//        return converter;
+//    }
 
+    /**
+     * jwt converter - 비대칭 키 sign
+     */
+
+    @Bean
+    public AccessTokenConverter jwtAccessTokenConverter() {
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new FileSystemResource("src/main/resources/oauth2jwt.jks"), "oaut2jwtpass".toCharArray());
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("oauth2jwt"));
+
+        return converter;
+    }
+
+
+    private class CustomUserDetailService implements UserDetailsService {
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            return null;
+        }
+    }
 }
